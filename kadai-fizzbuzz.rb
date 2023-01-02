a0062bf45f987cbf23c46cc6a1afa275d1b8c78d
shunsuke.taga <tagashunsuke@icloud.com>
2023-01-02 08:39:19 +0000

    homework submit

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,27 @@
+#fizzbuzz課題
+
+num_max = 100
+
+# (1..num_max).each do |num|
+# puts fizzbuzz(num)
+# end
+
+
+def fizzbuzz(num)
+  
+    if num % 15 == 0 
+      return 'FizzBuzz'
+    elsif num % 3 == 0 
+      return 'Fizz'
+    elsif num % 5 == 0 
+      return 'Buzz'
+    else
+      return num.to_s
+    end
+    
+end
+
+
+(1..num_max).each do |num|
+ puts fizzbuzz(num)
+end
